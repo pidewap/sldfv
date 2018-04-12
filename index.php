4599493dab968a7773982fada0432a5cb5dca0e6
pidewap <pidewap@gmail.com>
2018-04-13 04:56:52 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,18 @@
 <?php
-$dl=file_get_contents('https://www.saveitoffline.com/process/?url='.urlencode('http://www.youtube.com/watch?v='.$_GET['get-v']).'&type=json');
+echo '<html xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+    <title>Download Link</title>
+    <link rel="stylesheet" type="text/css" media="screen" href="http://satriamusic.com/css/css_css_style.css" />
+    </head>
+    <body><div class="container">';
+
+$dl=file_get_contents('https://www.saveitoffline.com/process/?url='.urlencode('http://www.youtube.com/watch?v='.$_GET['v']).'&type=json');
 $js=json_decode($dl,TRUE);
 $d=$js['urls'];
+echo '<h1 class="page-title">Download '.$js['title'].'</h1><div class="info">';
 for($i=0;$i<5;$i++){
-echo '
-									<li><a href="'.$d[$i]['id'].'" class="top">Download Video '.$d[$i]['label'].'</a></li>
-';
+echo '<a class="mp4" href="'.$d[$i]['id'].'" class="top">Download Video '.$d[$i]['label'].'</a><br>';
 }
+
+echo '</div></div></body></html>';
 ?>
