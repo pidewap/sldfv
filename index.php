d1f964404050ff21999b8818cd296ceef787d57f
pidewap <pidewap@gmail.com>
2018-04-13 08:41:40 +0700

    Update index.php

--- index.php
+++ index.php
@@ -3,8 +3,9 @@
 $dl=file_get_contents('https://www.saveitoffline.com/process/?url='.urlencode('http://www.youtube.com/watch?v='.$_GET['v']).'&type=json');
 $js=json_decode($dl,TRUE);
 $d=$js['urls'];
-echo '<h1 class="page-title">Download '.$js['urls'].'</h1>';
+echo '<h1 class="page-title">Download '.$js['title'].'</h1><ul>';
 for($i=0;$i<5;$i++){
-echo '<a class="mp4" href="'.$d[$i]['id'].'" class="top">Download Video '.$d[$i]['label'].'</a><br>';
+echo '<li><a class="mp4" href="'.$d[$i]['id'].'" class="top">Download Video '.$d[$i]['label'].'</a></li>';
 }
+echo '</ul>';
 ?>
