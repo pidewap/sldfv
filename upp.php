7fe65059079f1a5e211cf1532d59243dc77bdc8f
pidewap <pidewap@gmail.com>
2018-08-15 09:49:18 +0700

    Create upp.php

---
+++ upp.php
@@ -0,0 +1,18 @@
+
+<title>Remote upload made by wRock.Org</title>
+<center>
+<a href="http://www.wrock.org/" title="Sharing Latest Tips Tricks and Tutorials">
+			
+<img src="http://cdn.wrock.org/logo.png" alt="Sharing Latest Tips Tricks and Tutorials">
+		</a>
+	<br><p><br></p><form method="post" action="/uppp.php" >
+<input name="url" size="50" />
+<input name="submit" type="submit" />
+</form>
+
+<b>Instruction:</b>
+<p>Sample values for ftp and http</p>
+<p>ftp://username:password@example.com/path/to/file.png</p>
+<p>ftp://example.com/path/to/file.png</p>
+<p>http://www.example.com/path/to/file.png</p>
+<p><a href="http://www.wrock.org/">Sharing Latest Tips Tricks and Tutorials</a></p>
